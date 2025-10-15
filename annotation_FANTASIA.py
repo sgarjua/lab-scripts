b7794e011a0848282ddb4861e4ba1bf77be765b9
Sofía García Juan <sgarjua@gmail.com>
2025-10-15 15:57:10 +0200

    Update annotation_FANTASIA.py

--- annotation_FANTASIA.py
+++ annotation_FANTASIA.py
@@ -11,26 +11,49 @@
 import subprocess
 from pathlib import Path
 import csv, sys
+import os
 
 
 # configuración ===============================================================
 TSV = "/data/users/sgarjua/ann_diamond_test/species.tsv" # archivo con: nombre especie(=nombre carpeta) + ruta al fasta
 OUTDIR = Path("/data/users/sgarjua/SofiaFantasia/") # carpeta con las carpetas de las especies con los fastas
-
+GENERATE_GPSM = "/data/users/sgarjua/00_software/FANTASIA/generate_gopredsim_input_files.sh"
+LAUNCH_GPSM = "/data/users/sgarjua/00_software/FANTASIA/launch_gopredsim_pipeline.sh"
+GPU = "CUDA_VISIBLE_DEVICES=1"
 
 # funciones ===================================================================
 # limpiar el fasta
-def fasta_cleaner(fasta: Path, species: str, clean_fasta: str):
-    out = OUTDIR / species / clean_fasta
-    cmd = f"sed -r 's/ .+//' {fasta} > {out}"
-    print(cmd)
+def fasta_cleaner(fasta: Path, clean_fasta: Path):
 
-# crear carpeta
+    cmd = f"sed -r 's/ .+//' {fasta} > {clean_fasta}"
+    
+    #EJECUTAAAAAR, pero comprobar primero si ya existe
+
 
 # ejecutar primer comando
+def firt_step(clean_fasta: Path, prefix: str, fantasia_run: Path):
+
+     cmd = [ GENERATE_GPSM,
+            "--infile", clean_fasta, 
+            "--outpath", fantasia_run, 
+            "--prott5",
+            "--prefix", prefix,
+            "--mode GPU"
+            ]
+    print(cmd)
 
 # ejecutar segundo comando
+def second_step(prefix: str, fantasia_run: Path):
 
+    cmd = [ GPU,
+            F"screen -L -Logfile {prefix}.log",
+            LAUNCH_GPSM,
+            "-c", fantasia_run, 
+            "-x", prefix, 
+            "-m prott5",
+            "-o", fantasia_run
+            ]
+    print(cmd)
 
 # main ========================================================================
 def main():
@@ -69,16 +92,26 @@ def main():
                 prefix += parts[2][:3]
             
             # se limpia el fasta
-            clean_fasta = f"{Path(fasta).stem}.clean.faa"
+            clean_fasta_name = f"{Path(fasta).stem}.clean.faa"
+            clean_fasta = OUTDIR / species / clean_fasta
             fasta_cleaner(fasta, species, clean_fasta)
 
             # se crea la carpeta run_fantasia dentro de la carpeta de la especie
+            run_fantasia = OUTDIR / species / run_fantasia
+            # if not run_fantasia.exists():
+            #     out.mkdir(parents=True, exist_ok=True)
+            #     print("[INFO] Carpeta 'run_fantasia' creada correctamente")
 
             # nos movemos a esa carpeta
+            original = Path.cwd()
+            os.chdir(run_fantasia)
 
             # ejecución del primer comando
 
             # ejecución del segundo comando
+
+
+            os.chdir(original)
 
     print("\nTodo terminado. ✔")
 
