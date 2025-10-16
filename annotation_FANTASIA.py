f26e7678eb37c8103a32dc153a786067419cc5ac
Sofía García Juan <sgarjua@gmail.com>
2025-10-16 11:01:37 +0200

    Update annotation_FANTASIA.py

--- annotation_FANTASIA.py
+++ annotation_FANTASIA.py
@@ -54,7 +54,7 @@ def firt_step(clean_fasta: str, prefix: str, fantasia_run: str):
     else:
         print(f"[RUN] Se va a ejecutar el primer paso de FANTASIA")
         try:
-            subprocess.run(" ".join(cmd), shell=True, check=True)
+            subprocess.run(cmd, check=True)
             print(f"[DONE] Primer paso completado; ; cmd={" ".join(cmd)}")
         except subprocess.CalledProcessError as e:
             print(f"[FAIL] Revisa parámetros/rutas. Detalle: {e}")
