e85ab335da3ca056addabeeeb5e1fcd5c34f0845
Sofía García Juan <sgarjua@gmail.com>
2025-10-27 11:38:04 +0100

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -94,43 +94,65 @@ def append_fila(outfile: Path, row):
 
 def calc_total(outfile: Path):
     protes_h, protes_f, id_con_go_h, id_con_go_f, id_sin_go_h, id_sin_go_f, cobertura_h, cobertura_f, gos_por_prote_h, gos_por_prote_f, gos_totales_h, gos_totales_f, total_solapados, solape_h, solape_f = 0, 0 ,0 ,0 ,0 ,0 ,0, 0, 0, 0, 0, 0, 0, 0, 0
-
+    n = 0
     with outfile.open(encoding="utf-8") as tsv:
         for line in tsv:
             line = line.strip()
             if line.startswith("Especie"):
                 continue
             parts = line.split("\t")
+            n += 1
 
             protes = parts[1].split(" | ")
             protes_h += int(protes[0])
             protes_f += int(protes[1])
 
             id_con_go = parts[2].split(" | ")
-            id_con_go_h += int(id_con_go[0]) / 2
-            id_con_go_f += int(id_con_go[1]) / 2
+            id_con_go_h += int(id_con_go[0])
+            id_con_go_f += int(id_con_go[1])
 
             id_sin_go = parts[3].split(" | ")
-            id_sin_go_h += int(id_sin_go[0]) / 2
-            id_sin_go_f += int(id_sin_go[1]) / 2
+            id_sin_go_h += int(id_sin_go[0])
+            id_sin_go_f += int(id_sin_go[1])
 
             cobertura = parts[4].split(" | ")
-            cobertura_h += float(cobertura[0]) / 2.0
-            cobertura_f += float(cobertura[1]) / 2.0
+            cobertura_h += float(cobertura[0])
+            cobertura_f += float(cobertura[1])
 
             gos_por_prote = parts[5].split(" | ")
-            gos_por_prote_h += float(gos_por_prote[0]) / 2.0
-            gos_por_prote_f += float(gos_por_prote[1]) / 2.0
+            gos_por_prote_h += float(gos_por_prote[0])
+            gos_por_prote_f += float(gos_por_prote[1])
 
             gos_totales = parts[6].split(" | ")
             gos_totales_h += int(gos_totales[0])
             gos_por_prote_f += int(gos_totales[1])
 
-            total_solapados += int(parts[7]) / 2
+            total_solapados += int(parts[7])
 
             solape = parts[8].split(" | ")
-            solape_h += float(solape[0]) / 2.0
-            solape_f += float(solape[1]) / 2.0
+            solape_h += float(solape[0])
+            solape_f += float(solape[1])
+    
+
+    id_con_go_h += id_con_go_h / n
+    id_con_go_f += id_con_go_f / n
+
+    id_sin_go_h += id_sin_go_h / n
+    id_sin_go_f += id_sin_go_f / n
+
+    cobertura_h += cobertura_h / n
+    cobertura_f += cobertura_f / n
+
+    gos_por_prote_h += gos_por_prote_h / n
+    gos_por_prote_f += gos_por_prote_f / n
+
+    gos_totales_h += gos_totales_h / n
+    gos_por_prote_f += gos_totales_f / n
+
+    total_solapados += total_solapados / n
+
+    solape_h += solape_h / n
+    solape_f += solape_f / n
     fila = [
         "MEDIA",
         f"{protes_h} | {protes_f}",
